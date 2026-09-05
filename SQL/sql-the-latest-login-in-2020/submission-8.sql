c1dc23bf3a9d9cdc074cc7b27c809bd10939d70c
Raghav Nilesh Nanavati <rnanavati2025@gmail.com>
2026-09-05 18:52:18 -0400

    Add: sql-the-latest-login-in-2020 - submission-8

---
+++ SQL/sql-the-latest-login-in-2020/submission-8.sql
@@ -0,0 +1,5 @@
+-- Write your query below
+SELECT user_id,MAX(time_stamp) as last_stamp
+FROM logins
+WHERE time_stamp >= '2020-01-01 00:00:00' AND time_stamp < '2021-01-01 00:00:00'
+GROUP BY user_id;
